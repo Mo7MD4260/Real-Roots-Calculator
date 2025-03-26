55158845f1cc8cdafdc3d17544cfd81c92085122
MO7AMMED <mohamed_1555@ai.kfs.edu.eg>
2025-03-26 17:36:25 +0200

    main.py

--- RealRootsCalculator_project_sourceCode/main.py
+++ RealRootsCalculator_project_sourceCode/main.py
@@ -157,7 +157,7 @@ def animate_gif(label, gif, frame=0):
         label.config(image=frame_image)
         label.image = frame_image
 
-        # الانتقال إلى الإطار التالي بعد 100 مللي ثانية (سرعة الحركة)
+        # الانتقال إلى الإطار التالي بعد 5 مللي ثانية (سرعة الحركة)
         label.after(5, animate_gif, label, gif, (frame + 1) % gif.n_frames)
     except Exception:
         pass  # في حالة انتهاء الصورة
